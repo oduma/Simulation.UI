9479fc309fe4e450fe16a770c505028c6aa262da
oduma <tavi_duma2002@yahoo.co.uk>
2012-07-05 08:08:24 +0100

    everything working somehow in unit-test

--- Tests/UITests/autovote/iocplugin.js
+++ Tests/UITests/autovote/iocplugin.js
@@ -5,53 +5,60 @@ ScoreAlgorythmType.prototype = {
     algorythm: function () {  }
 };
 
+ScoreData = function () { };
+ScoreData.prototype = {
+    rank:0,
+    score:0
+};
 
-(function($) {
-    var methods={
-        init:function(options){
+(function ($) {
+    var methods = {
+        init: function (options) {
             return this.each(function () {
-                var currentSelection=$(this);
-                if(!currentSelection.data('algSelector.'+options.itemType)){
-                    currentSelection.data('algSelector.'+options.itemType,options.algorythms);
-                //Add the event if selection is changed to set the inUse flag
-                 $(this).bind('change.algSelector', methods.selectAlgorythm);
+                var currentSelection = $(this);
+                if (!currentSelection.data(this.id)) {
+                    currentSelection.data(this.id, options.algorythms);
+                    //Add the event if selection is changed to set the inUse flag
+                    $(this).bind('change.' + this.id, methods.selectAlgorythm);
                 }
             });
         },
-        destroy:function(){
-        
-       return this.each(function(){
-         $(this).unbind('.algSelector');
-       })
+        destroy: function () {
+
+            return this.each(function () {
+                $(this).unbind('.' + this.id);
+            })
         },
-        selectAlgorythm: function(e)
-        {
-            alert('I got here');
+        selectAlgorythm: function (e) {
+            var selectedOption = $("#" + this.id + " option:selected").val();
+            $.each($(this).data(this.id), function (i, alg) {
+                alg.inUse = false;
+                if (alg.name === selectedOption)
+                    alg.inUse = true;
+            });
         },
-        run:function(itemType, result){
-            return this.each(function() {
-                var currentSelection=$(this);
-                if(!currentSelection.data('algSelector.' + itemType))
+        run: function (scoreData) {
+            return this.each(function () {
+                var currentSelection = $(this);
+                if (!currentSelection.data(this.id))
                     $.error('algSelector not initialized');
-                var algs=currentSelection.data('algSelector.' + itemType);
-                for(i=0;i<algs.length;i++)
-                {
-                    if(algs[i].inUse)
-                        result.returnValue=algs[i].algorythm();
-                }
+                $.each(currentSelection.data(this.id),function(i, alg){
+                    if(alg.inUse)
+                        alg.algorythm(scoreData);
+                });
             });
         }
     };
 
-    $.fn.algSelector=function(method){
-        if(methods[method]){
-            return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    $.fn.algSelector = function (method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
         }
-        else if(typeof method==='object' || !method){
-            return methods.init.apply( this, arguments);
+        else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
         }
-        else{
-            $.error("Method "+ method +" does not exist in jQuery.selector");
+        else {
+            $.error("Method " + method + " does not exist in jQuery.selector");
         }
     };
 
--- Tests/UITests/autovote/iocplugintests.js
+++ Tests/UITests/autovote/iocplugintests.js
@@ -1,39 +1,40 @@
 ﻿test("Selector plugin tests", function () {
 
+    var scoreData = new ScoreData();
     var options;
     var abc = new ScoreAlgorythmType();
     abc.name = "abc";
     abc.inUse = false;
-    abc.algorythm = function () { return 1 + 1; };
+    abc.algorythm = function (scoreData) { scoreData.score = scoreData.rank + 1; };
 
     var def = new ScoreAlgorythmType();
     def.name = "def";
     def.inUse = true;
-    def.algorythm = function () { return 1 + 2; };
+    def.algorythm = function (scoreData) { scoreData.score = scoreData.rank + 2; };
 
     var feg = new ScoreAlgorythmType();
     feg.name = "feg";
     feg.inUse = false;
-    feg.algorythm = function () { return 1 + 3; };
+    feg.algorythm = function (scoreData) { scoreData.score = scoreData.rank + 3; };
 
-    options = { "itemType": "itemType1", "algorythms": [abc, def, feg] };
+    options = { "algorythms": [abc, def, feg] };
 
 
     ok($("#algSelector").algSelector(options) != null, 'I can call it at least');
-    equal(3, $("#algSelector").data('algSelector.itemType1').length, "The Data is Ok");
-    equal("abc", $("#algSelector").data('algSelector.itemType1')[0].name, "First One in data is Ok");
-    equal("def", $("#algSelector").data('algSelector.itemType1')[1].name, "Second One in data is Ok");
-    equal("feg", $("#algSelector").data('algSelector.itemType1')[2].name, "Third One in data is Ok");
-    equal(2, $("#algSelector").data('algSelector.itemType1')[0].algorythm(), "First Alg in data is Ok");
-    equal(3, $("#algSelector").data('algSelector.itemType1')[1].algorythm(), "Second Alg in data is Ok");
-    equal(4, $("#algSelector").data('algSelector.itemType1')[2].algorythm(), "Third Alg in data is Ok");
-    ok(!$("#algSelector").data('algSelector.itemType1')[0].inUse, "First One not in use");
-    ok($("#algSelector").data('algSelector.itemType1')[1].inUse, "Second One in use");
-    ok(!$("#algSelector").data('algSelector.itemType1')[2].inUse, "Third One not in use");
-    function results() { this.returnValue = 0; };
-    $("#algSelector").algSelector("run", "itemType1", results);
-    equal(3, results.returnValue, "Execution of run is Ok");
+    equal(3, $("#algSelector").data('algSelector').length, "The Data is Ok");
+    equal("abc", $("#algSelector").data('algSelector')[0].name, "First One in data is Ok");
+    equal("def", $("#algSelector").data('algSelector')[1].name, "Second One in data is Ok");
+    equal("feg", $("#algSelector").data('algSelector')[2].name, "Third One in data is Ok");
+    ok(!$("#algSelector").data('algSelector')[0].inUse, "First One not in use");
+    ok($("#algSelector").data('algSelector')[1].inUse, "Second One in use");
+    ok(!$("#algSelector").data('algSelector')[2].inUse, "Third One not in use");
+    scoreData.rank = 1;
+    $("#algSelector").algSelector("run", scoreData);
+    equal(3, scoreData.score, "Execution of run is Ok");
     $("#algSelector option")[0].selected = true;
     $("#algSelector option")[1].selected = false;
     $("#algSelector").change();
+    $("#algSelector").algSelector("run", scoreData);
+    equal(2, scoreData.score, "Execution of run is Ok");
+
 });
